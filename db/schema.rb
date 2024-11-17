7c8d028eb4f1ac8dc6c8aaebfc1eaa54fd63ec76
will wedmedyk <willwedmedyk@gmail.com>
2024-11-17 09:07:45 -0500

    update for render deploy

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2024_11_16_135818) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -22,7 +25,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_16_135818) do
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
     t.text "body"
-    t.integer "article_id", null: false
+    t.bigint "article_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
